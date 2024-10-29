6386e806388fcdf801f6b70d2fc50c008a46bd8a
Overwrite987 <artem.korolev_2003@mail.ru>
2024-10-29 03:25:29 +0300

    remove unused import

--- src/main/java/ru/overwrite/ublocker/listeners/commands/ConsoleBlocker.java
+++ src/main/java/ru/overwrite/ublocker/listeners/commands/ConsoleBlocker.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
