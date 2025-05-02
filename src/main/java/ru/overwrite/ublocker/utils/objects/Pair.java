bf8d4f1cbff0a76b2d71c1629b80c79c4836fede
Overwrite987 <artem.korolev_2003@mail.ru>
2025-05-02 13:08:10 +0300

    use WORKING pair

--- src/main/java/ru/overwrite/ublocker/utils/objects/Pair.java
+++ src/main/java/ru/overwrite/ublocker/utils/objects/Pair.java
@@ -1,65 +1,31 @@
-// Stolen from it.unimi.dsi.fastutil.Pair
 package ru.overwrite.ublocker.utils.objects;
 
-import java.util.Comparator;
+import lombok.AllArgsConstructor;
 
-public interface Pair<L, R> {
+@AllArgsConstructor
+public class Pair<L, R> {
 
-    L left();
+    private L left;
+    private R right;
 
-    R right();
-
-    default Pair<L, R> left(final L l) {
-        throw new UnsupportedOperationException();
+    public L left() {
+        return this.left;
     }
 
-    default Pair<L, R> right(final R r) {
-        throw new UnsupportedOperationException();
+    public R right() {
+        return this.right;
     }
 
-    default L first() {
-        return left();
+    public void left(final L l) {
+        this.left = l;
     }
 
-    default R second() {
-        return right();
+    public void right(final R r) {
+        this.right = r;
     }
 
-    default Pair<L, R> first(final L l) {
-        return left(l);
-    }
-
-    default Pair<L, R> second(final R r) {
-        return right(r);
-    }
-
-    default Pair<L, R> key(final L l) {
-        return left(l);
-    }
-
-    default Pair<L, R> value(final R r) {
-        return right(r);
-    }
-
-    default L key() {
-        return left();
-    }
-
-    default R value() {
-        return right();
-    }
-
-    static <L, R> Pair<L, R> of(final L l, final R r) {
-        return new ImmutablePair<>(l, r);
-    }
-
-    @SuppressWarnings("unchecked")
-    static <L, R> Comparator<it.unimi.dsi.fastutil.Pair<L, R>> lexComparator() {
-        return (x, y) -> {
-            final int t = ((Comparable<L>)x.left()).compareTo(y.left());
-            if (t != 0) return t;
-            return ((Comparable<R>)x.right()).compareTo(y.right());
-        };
+    public static <L, R> Pair<L, R> of(final L l, final R r) {
+        return new Pair<>(l, r);
     }
 }
 
